10b49beb541cfec387eacf4e0b6853c4acc04142
ErwinAdriel <adrielquispe87@gmail.com>
2024-08-13 23:19:34 -0300

    Instalando Tailwind

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import tw from 'twrnc';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw`text-black justify-center items-center flex-1`}>
+      <Text style={tw`text-3xl`}>Bienvenidos</Text>
       <StatusBar style="auto" />
     </View>
   );
